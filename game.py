9d7fae12cfa15a2266069043bf4b26600b063896
jakelikeatree <007jakeb@gmail.com>
2023-07-06 21:18:44 +0100

    let the games begin!

---
+++ game.py
@@ -0,0 +1,6 @@
+# generate a random number between 1-100
+# create a loop to ask the plyer for a guess
+# test if the gess is correct
+# if its too low or high, give user a hint
+# if its correct =, let them know and end the game
+
