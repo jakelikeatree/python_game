7f08806817b58107df148ce73bef146db2443c48
jakelikeatree <007jakeb@gmail.com>
2023-07-07 13:21:37 +0100

    what

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
 # generate a random number between 1-100
-# create a loop to ask the plyer for a guess
-# test if the gess is correct
-# if its too low or high, give user a hint
-# if its correct =, let them know and end the game
+random number = random(1-101) 
+print(random number)
 
-print("hello")
+# create a loop to ask the plyer for a guess
+
+
+# test if the gess is correct
+
+
+# if its too low or high, give user a hint
+
+
+# if its correct =, let them know and end the game
